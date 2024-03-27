89d5781a5b3500792f720cca2148b9db41b085ec
Tesloboy <meex_v92@mail.ru>
2024-03-27 22:53:08 +0300

    Initial Commit

---
+++ VirusSim/VirusSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirusSimApp.swift
+//  VirusSim
+//
+//  Created by Viktor Teslenko on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct VirusSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
